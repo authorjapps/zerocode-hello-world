0e2aa17970faf7adcd7a75c71efcd665983cd209
Author jSmart <author.japps@gmail.com>
2019-03-30 23:14:12 +0000

    ISS-226 - JUnit5 tests

---
+++ src/main/java/org/jsmart/zerocode/junit5/Calculator.java
@@ -0,0 +1,9 @@
+package org.jsmart.zerocode.junit5;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/org/jsmart/zerocode/junit5/FirstTest.java
@@ -0,0 +1,36 @@
+package org.jsmart.zerocode.junit5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("fast")
+class FirstTest {
+
+	@Test
+	@DisplayName("My 1st JUnit 5 test! 😎")
+	void myFirstTest(TestInfo testInfo) {
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+		assertEquals("My 1st JUnit 5 test! 😎", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+			"1,    2,   3",
+			"2,    1,   3",
+			"30,  70, 100",
+			"1,  99, 100"
+	})
+	void add(int first, int second, int expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+
+}
